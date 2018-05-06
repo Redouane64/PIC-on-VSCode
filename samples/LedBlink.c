d06d67ea18b4edfc8930cc46e088af520bd1ed30
Redouane Sobaihi <s-redouane@outlook.com>
2018-05-06 18:50:56 +0100

    Create example.

---
+++ samples/LedBlink.c
@@ -0,0 +1,19 @@
+
+#include <xc.h>
+#include <pic16f84a.h>
+
+#define _XTAL_FREQ 1000000 // 1Mhz
+
+void main() {
+    
+    TRISB = 0x00;
+
+    while(1) {
+        PORTB = 0x01;
+        __delay_ms(250);
+        PORTB = 0x00;
+        __delay_ms(250);
+    }
+
+    
+}
